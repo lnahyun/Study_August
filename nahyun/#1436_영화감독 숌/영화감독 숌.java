7798cdf275d87e728c29c4426942a522461bbd8c
lnahyun <164346626+lnahyun@users.noreply.github.com>
2024-08-08 18:19:23 +0900

    [ADD] 영화감독 숌.java

---
+++ nahyun/#1436_영화감독 숌/영화감독 숌.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int num = 666;
+        int count = 1;
+
+        while(count != N) {
+            num++;
+            if(Integer.toString(num).contains("666"))
+                count++;
+        }
+
+        System.out.println(num);
+    }
+}

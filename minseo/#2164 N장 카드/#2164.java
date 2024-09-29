1a98bb7a0202b83b34863fbcc6e6dc42193e4d21
chominseo0723 <165632548+chominseo0723@users.noreply.github.com>
2024-09-29 13:53:07 +0900

    Create #2164.java

---
+++ minseo/#2164 N장 카드/#2164.java
@@ -0,0 +1,24 @@
+package java_practice;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _2164 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> q = new LinkedList<Integer>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i< N; i++){
+            q.offer(i);
+        }
+        while(q.size() > 1){
+            q.poll();
+            q.offer(q.poll());
+        }
+        System.out.println(q.poll()+1);
+    }
+}
